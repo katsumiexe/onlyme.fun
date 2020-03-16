12695335d925aa39689e04ebdeca73071e762e18
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-03-16 18:05:41 +0900

    ar

--- qr_test.php
+++ qr_test.php
@@ -2,28 +2,18 @@
 include_once("./library/no_session.php");
 
 $img_url 	= imagecreatefromjpeg("./myalbum/qr.jpg");
-$base		=imagecreate(108,108);
+$base		=imagecreate(120,120);
 $black		=ImageColorAllocate($base,0,0,0);
 $white		=ImageColorAllocate($base,255,255,255);
 $red		=ImageColorAllocate($base,255,0,0);
 
+ImageFilledRectangle($base,0,0,120,120,$white);
+
 $ck=0;
 for($y=0;$y<36;$y++){
 	for($x=0;$x<36;$x++){
-
-		if($x>18){
-			$xp=20;
-
-		}else{
-			$xp=10;
-		}
-
-		if($y>18){
-			$yp=20;
-
-		}else{
-			$yp=10;
-		}
+		$xp=13;
+		$yp=13;
 
 		$pixel = ImageColorAt($img_url, $x*25+$xp, $y*25+$yp);
 		
@@ -39,10 +29,10 @@ for($y=0;$y<36;$y++){
 	for($x=0;$x<36;$x++){
 		$code_ck=substr($s_code,$ck,1);
 
-		$x1=$x*3;
-		$x2=$x*3+2;
-		$y1=$y*3;
-		$y2=$y*3+2;
+		$x1=$x*3+6;
+		$x2=$x*3+6+2;
+		$y1=$y*3+6;
+		$y2=$y*3+6+2;
 
 		if($code_ck == 1){
 			imagefilledrectangle($base, $x1, $y1, $x2, $y2, $black);
