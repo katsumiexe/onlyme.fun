f5627ca74324ccebbbeaafe82aec257670428af3
katsumiexe <counterpost2016@gmail.com>
2020-03-17 14:01:49 +0900

    ar

--- qr_test.php
+++ qr_test.php
@@ -1,6 +1,14 @@
 <?
 include_once("./library/no_session.php");
 
+$at=array(14,39,65,90,118,137,167);
+
+$at_lim=array(0,27,51,78,102,129,153);
+
+$at_point=array(27,24,27,24,27,24,27);
+
+
+
 $img_url 	= imagecreatefromjpeg("./myalbum/qr.jpg");
 $base		=imagecreate(120,120);
 $black		=ImageColorAllocate($base,0,0,0);
@@ -10,12 +18,18 @@ $red		=ImageColorAllocate($base,255,0,0);
 ImageFilledRectangle($base,0,0,120,120,$white);
 
 $ck=0;
-for($y=0;$y<36;$y++){
-	for($x=0;$x<36;$x++){
-		$xp=13;
-		$yp=13;
+for($y=0;$y<35;$y++){
+		$y_pt=$y%7;
+		$y_point=($y_point+$at_point[$y_pt]);
+		$x_point="";
 
-		$pixel = ImageColorAt($img_url, $x*25+$xp, $y*25+$yp);
+	for($x=0;$x<35;$x++){
+		$x_pt=$x%7;
+		$x_point=($x_point+$at_point[$x_pt]);
+
+//echo $x_point."<br>\n";
+
+		$pixel = ImageColorAt($img_url, $x_point-10, $y_point-10);
 		
 		if($pixel==0){
 			$s_code.=1;		
@@ -24,9 +38,9 @@ for($y=0;$y<36;$y++){
 		}
 	}
 }
-
-for($y=0;$y<36;$y++){
-	for($x=0;$x<36;$x++){
+print($s_code);
+for($y=0;$y<35;$y++){
+	for($x=0;$x<35;$x++){
 		$code_ck=substr($s_code,$ck,1);
 
 		$x1=$x*3+6;
@@ -35,7 +49,7 @@ for($y=0;$y<36;$y++){
 		$y2=$y*3+6+2;
 
 		if($code_ck == 1){
-			imagefilledrectangle($base, $x1, $y1, $x2, $y2, $black);
+			imagefilledrectangle($base, $x1, $y1, $x2, $y2, $red);
 //		    ImageSetPixel($base,$x,$y,$red); 
 
 		}else{
