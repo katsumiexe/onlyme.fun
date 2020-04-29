52e04f20cfefdc31c913f071ebd1c67de7bcb7d7
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2020-04-29 18:22:33 +0900

    ar

--- chg.php
+++ chg.php
@@ -1,9 +1,22 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+mb_language("Japanese");
+mb_internal_encoding("UTF-8");
+
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/POP3.php';
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/OAuth.php';
+require 'PHPMailer/language/phpmailer.lang-ja.php';
+
 include_once("./library/lib.php");
 include_once("./library/lib_me.php");
 include_once("./library/session.php");
-$t_date=date("Y-m-d H:i:s");
+include_once("./library/lib_regist.php");
 
+$t_date=date("Y-m-d H:i:s");
 $p_name		=$_POST["p_name"];
 $p_mail		=$_POST["p_mail"];
 $p_pass		=$_POST["p_pass"];
@@ -15,17 +28,43 @@ $p_code=substr("0000000000".$code,-12);
 $sql ="INSERT INTO me_config_chg (`date`,`user_id`,`name`,`mail`,`pass`,`state`,`code`)";
 $sql.="VALUES('{$t_date}','{$user["id"]}','{$p_name}','{$p_mail}','{$p_pass}','{$p_state}','{$p_code}')";
 mysqli_query($mysqli,$sql);
+
+	$msg=file_get_contents("./mail/mail_3.txt");
+	$msg=str_replace("[code]",$p_code,$msg);
+	$msg=str_replace("[name]",$p_name,$msg);
+
+	$mailer = new PHPMailer();
+	$mailer->IsSMTP();
+
+	$mailer->Host		= $host;
+	$mailer->CharSet	= 'utf-8';
+	$mailer->SMTPAuth	= TRUE;
+	$mailer->Username	= $mail_from;
+	$mailer->Password	= $mail_pass;
+	$mailer->SMTPSecure = 'tls';
+	$mailer->Port		= 587;
+//	$mailer->SMTPDebug = 2;
+
+	$mailer->From     = $mail_from;
+	$mailer->FromName = mb_convert_encoding("写真名刺作成サイト★OnlyMe","UTF-8","AUTO");
+	$mailer->Subject  = mb_convert_encoding('会員登録変更',"UTF-8","AUTO");
+	$mailer->Body     = mb_convert_encoding($msg,"UTF-8","AUTO");
+	$mailer->AddAddress($me_mail);
+
+/*
 $to      = $p_mail;
 $subject = "写真名刺簡単作成★OnlyMe";
 $message = "下記リンクよりアクセスし、変更を完了させてください\n\nhttps://onlyme.fun/chg2.php?code=".$p_code."\n\n※登録後30分以上経過しますと無効となります。";
 $headers = 'From: staff@onlyme.fun' . "\r\n";
 mb_send_mail($to, $subject, $message, $headers);
+*/
+
 
 $_SESSION = array();
 $user = array();
 session_destroy(); 
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE HTML>
 <html lang="ja">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
